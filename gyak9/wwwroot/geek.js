0910907c22da69ca01a085a70b9b7cd0f310d202
Lencse22 <marcell.lencses@stud.uni-corvinus.hu>
2022-11-23 12:07:28 +0100

    Munka

---
+++ gyak9/wwwroot/geek.js
@@ -0,0 +1,25 @@
+﻿var viccek;
+
+fetch("/questions.json")
+    .then(r => r.json())
+    .then(d => adatÉrkezett(d));
+
+function letöltés(adat) {
+    let ide = document.getElementById("ide");
+    console.log(`${adat.length} kérdés érketett`)
+    letöltésBefejeződött(d)
+}
+
+
+
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        console.log(d)
+        viccek = d;
+        for (var i = 0; i < viccek.length; i++) {
+            console.log(viccek[i].questionText)
+            let elem = document.createElement("li")
+            elem.innerHTML = viccek[i].questionText
+            ide.appendChild(elem)
+        }
+    }
